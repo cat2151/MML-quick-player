6406239d09b652e6492675c39105d9d5a4ccf544
cat2151 <fct_2004@mail.goo.ne.jp>
2018-05-06 18:02:13 +0900

    chromeで鳴るようにした。具体的には、Sionic.js を組み込んだ。

--- controller.js
+++ controller.js
@@ -2,7 +2,10 @@ angular.module('generatorApp')
 .controller('generatorController', ['$scope', '$location', '$timeout', 'GeneratorService',
 function($scope, $location, $timeout, GeneratorService) {
 
+  $scope.inputText = '';
   $scope.generatedMml = "なし";
+  $scope.mmlFormat = "sion";
+  $scope.iPhoneReady = false;
 
   function setMmlFromUrl() {
     // [URLイメージ] ～/#?mml=C
@@ -14,22 +17,69 @@ function($scope, $location, $timeout, GeneratorService) {
   }
 
   $scope.generate = function() {
+    //iOSでは、WebAudioを最初にclickイベントから扱う必要がある。
+    //一度onloadまたはonchangedで実行してしまうと、その後clickで再生しても無音になる。
+    if($scope.isiPhone() && $scope.iPhoneReady === false){
+        return;
+    }
+
+    $timeout(function() { // compileより前にする(compileがSIOPMロード失敗の為にundefinedでexceptionになっても、先にURLへの反映はしておく)
+      setParamsToUrlFromScope();
+    }, 0);
+
     $scope.generatedMml = $scope.inputText;
-    SIOPM.compile($scope.generatedMml);
-    // URLに反映 [用途] 書いたMMLをURLコピペで共有できるようにする
-    $location.search({mml : $scope.generatedMml});
+    try{
+      SIOPM.stop();
+    }catch(e){
+      console.log(e);
+    }
+    Pico.pause();
+
+    switch($scope.mmlFormat){
+      case "sion" :
+        SIOPM.compile($scope.generatedMml);
+        break;
+      case "sionic" :
+        Pico.play(Sionic($scope.generatedMml));
+        break;
+      default:
+        console.error("Unsupported format");
+    }
+
   };
+
+  // URLに反映 [用途] 書いたMMLをURLコピペで共有できるようにする
+  function setParamsToUrlFromScope() {
+    $location.search({
+      mml : $scope.generatedMml
+    });
+  }
+
+  $scope.play = function(){
+    $scope.iPhoneReady = true;
+    $scope.generate();
+  }
+
+  $scope.isiPhone = function(){
+    return window.navigator.userAgent.toLowerCase().indexOf("iphone") >= 0;
+  }
 
   SIOPM.onLoad = function() {
     $timeout(function() {
       setMmlFromUrl();
     }, 1000);
   };
-        
+
   SIOPM.onCompileComplete = function() {
     SIOPM.play();
   };
 
-  SIOPM.initialize();
+  try{
+    SIOPM.initialize(); // [前提] SIOPMのプロパティへ各functionを代入し終わっていること
+  }catch(e){
+    //fallback
+    $scope.mmlFormat = "sionic";
+    $scope.generate();
+  }
 
 }]);
